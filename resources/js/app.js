505b73cb9aa18e89ebfb419eb595a7b32459452b
Marvin Baga <mjdbaga@gmail.com>
2024-03-08 20:51:46 +0800

    feat: fix layout for boarder form

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,8 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+Alpine.store('boarder-tabs', {
+    current: 'personal',
+    items: ['personal', 'contact', 'type']
+});
--- resources/views/components/content-wrap.blade.php
+++ resources/views/components/content-wrap.blade.php
@@ -1,9 +1,7 @@
 <div {!! $attributes->merge([
-    'class' => 'py-12 mx-auto sm:px-6 lg:px-8',
+    'class' => 'py-12 mx-auto bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg',
 ]) !!}>
-    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-        <div class="p-6 text-gray-900 dark:text-gray-100">
-            {{ $slot }}
-        </div>
+    <div class="sm:px-6 lg:px-8 text-gray-900 dark:text-gray-100">
+        {{ $slot }}
     </div>
 </div>
