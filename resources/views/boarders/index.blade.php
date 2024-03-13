8ec01f135bf9e3d93c4c9de357fbc0f9d1a1a250
Marvin Baga <mjdbaga@gmail.com>
2024-03-13 14:14:34 +0800

    feat: add delete forms for boarders

--- resources/views/boarders/index.blade.php
+++ resources/views/boarders/index.blade.php
@@ -1,16 +1,21 @@
+@section('page_title', 'Boarders')
 <x-app-layout>
     <x-page-heading :title="'Boarders'" :actions="[['href' => 'boarders.create', 'title' => 'Create New Boarder']]" />
 
-    <x-content-wrap class="max-w-7xl">
+    <x-content-wrap class="max-w-7xl mt-12">
         <x-heading-entry :headings="['Name', 'Email', 'Last Updated', 'Actions']" />
         @forelse ($boarders as $boarder)
             <x-row-entry :columns="4">
-                <div>{{ $boarder->first_name }} {{ $boarder->last_name }}</div>
+                <div>{{ $boarder->fullName }}</div>
                 <div>{{ $boarder->email }}</div>
                 <div>{{ $boarder->updated_at->diffForHumans() }}</div>
-                <div class="flex gap-2">
+                <div class="flex gap-2 justify-end">
                     <x-link-button :href="route('boarders.edit', $boarder->id)">Edit</x-link-button>
-                    <button>Delete</button>
+                    <form action="{{ route('boarders.destroy', $boarder) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <x-danger-button class="btn btn-delete">Delete</x-danger-button>
+                    </form>
                 </div>
             </x-row-entry>
         @empty
