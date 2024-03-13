863f54df653215c960bb85467cc90e7e238cbcc0
Marvin Baga <mjdbaga@gmail.com>
2024-03-13 14:16:30 +0800

    feat: add dynamic titles

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,11 @@
                         {{ __('Boarders') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('units.index')" :active="request()->routeIs('units.index')">
+                        {{ __('Units') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
