b36fb4997bde956e4e6a57bfd1e67e9a934b7256
Marvin Baga <mjdbaga@gmail.com>
2024-03-13 14:15:32 +0800

    feat: convert link component class to css tailwind component

--- resources/views/components/link-button.blade.php
+++ resources/views/components/link-button.blade.php
@@ -1,4 +1,3 @@
-<a href="{{ $href }}"
-    {{ $attributes->class('rounded-md border border-slate-300 bg-white px-2.5 py-1.5 text-center text-sm font-semibold text-black shadow-sm hover:bg-slate-100 ease-in duration-100') }}>
+<a href="{{ $href }}" {{ $attributes->class('btn') }}>
     {{ $slot }}
 </a>
